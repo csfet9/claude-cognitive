c7573866013ad727c4b587470ec9fd529b473d97
csfet9 <claudiusfetan@gmail.com>
2026-01-03 18:36:11 +0100

    Update CLI VERSION to 0.2.6

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -26,7 +26,7 @@ import { registerProcessSessionCommand } from "../hooks/process-session.js";
 import { registerBufferMessageCommand } from "../hooks/buffer-message.js";
 
 // Get version from package.json
-const VERSION = "0.2.5";
+const VERSION = "0.2.6";
 
 /**
  * Create and configure the CLI.
